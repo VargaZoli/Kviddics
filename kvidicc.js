8e1357ff6a879a578282e2ac6d256b4e639baf25
VargaZoli <75310585+VargaZoli@users.noreply.github.com>
2021-05-06 13:00:12 +0200

    Add files via upload

---
+++ kvidicc.js
@@ -0,0 +1,8 @@
+function add(){
+    var a,b,c;
+    a=Number(document.getElementById("first").value);
+    b=Number(document.getElementById("second").value);
+    c= a*10 + b*150;
+    document.getElementById("answer").value= c;
+    
+    }
